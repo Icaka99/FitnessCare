51863fac1a0684ec20b7b03ec43e445db1b996ef
Hristo <kisimovhristo@gmail.com>
2020-12-04 12:07:53 +0200

    Implement Add Article Picture

---
+++ Data/FitnessCare.Data/Migrations/20201204095811_AddArticleImageUrl.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FitnessCare.Data.Migrations
+{
+    public partial class AddArticleImageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Articles");
+        }
+    }
+}
