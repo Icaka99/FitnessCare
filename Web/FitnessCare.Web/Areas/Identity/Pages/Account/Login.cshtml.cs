31bf37ad2269fe8a668936a8a9c0601a4f5d95ec
Hristo <kisimovhristo@gmail.com>
2020-12-13 11:10:14 +0200

    Change default Email login with Username login

--- Web/FitnessCare.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Web/FitnessCare.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -44,8 +44,8 @@ namespace FitnessCare.Web.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+            public string Username { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
@@ -82,7 +82,7 @@ namespace FitnessCare.Web.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
